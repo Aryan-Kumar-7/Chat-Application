d8015b4d9659650b26d97e106f97eaa17c655c2d
Aryan-Kumar-7 <rajaryankumar26april@gmail.com>
2024-03-07 14:43:33 +0530

    Backend part

---
+++ backend/controllers/message.controllers.js
@@ -0,0 +1,66 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
+export const sendMessage = async (req, res) => {
+    try {
+        const {message} = req.body;
+        const { id: reciverId } = req.params;
+        const senderId = req.user._id;
+        
+        let conversation = await Conversation.findOne({
+            participants: { $all: [senderId, reciverId]},
+        });
+
+        if(!conversation) {
+            conversation = await Conversation.create({
+                participants: [senderId, reciverId],
+            });
+        }
+
+        const newMessage = new Message({
+            senderId,
+            reciverId,
+            message,
+        });
+
+        if(newMessage) {
+            conversation.message.push(newMessage._id);
+        }
+
+        // SOKET TO functionality will go here
+
+        // await conversation.save();
+        // await newMessage.save();
+
+        await Promise.all([conversation.save(), newMessage.save()]);
+
+        res.status(201).json(newMessage);
+
+    } catch (error) {
+        console.log("Error in sendMessage controller: ", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+};
+
+
+export const getMessage = async (req, res) => {
+    try {
+        
+        const {id: userToChatId} = req.params;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChatId]},
+        }).populate("message"); // NOT REFERENCE BUT ACTUAL MESSAGES
+
+        if(!conversation) return res.status(200).json([]);
+
+        const message = conversation.message;
+
+        res.status(200).json(conversation.message);
+
+    } catch (error) {
+        console.log("Error in getMessage controller: ", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+}
